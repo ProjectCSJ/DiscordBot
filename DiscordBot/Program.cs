b33a751080213a1725cf448eada8b0c40ff7aade
友人Ｓ <YuuJinS@users.noreply.github.com>
2022-05-18 18:22:28 +0900

    Setup Bot

--- DiscordBot/Program.cs
+++ DiscordBot/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+
+var token = "YOUR BOT TOKEN";
+var client = new DiscordSocketClient();
+
+client.Log += LogAsync;
+client.Ready += OnReady;
+client.MessageReceived += OnReceiveMessage;
+client.SlashCommandExecuted += OnReceiveSlashCommand;
+
+await client.LoginAsync(TokenType.Bot, token);
+await client.StartAsync();
+
+Task LogAsync(LogMessage message)
+{
+    if (message.Exception is CommandException cmdException)
+    {
+        Console.WriteLine($"[Command/{message.Severity}] {cmdException.Command.Aliases.First()}"
+            + $" failed to execute in {cmdException.Context.Channel}.");
+        Console.WriteLine(cmdException);
+    }
+    else
+        Console.WriteLine($"[General/{message.Severity}] {message}");
+
+    return Task.CompletedTask;
+}
+
+async Task OnReady() { }
+async Task OnReceiveMessage(SocketMessage msg) { }
+async Task OnReceiveSlashCommand(SocketSlashCommand cmd) { }
